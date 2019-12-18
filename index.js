bc2593084b3855ebbb626e61bc27b8e8009010cb
Mamessile Kiman <sile.kiman@yahoo.com>
2019-12-18 00:20:18 -0500

    udpaded the code to change the name color

--- index.js
+++ index.js
@@ -7,17 +7,18 @@ const writeFileAsync = util.promisify(fs.writeFile);
 
 
  //declare some varaible
-let user_picture;
-let user_name;
-let user_location ;
-let user_profile ;
-let user_followers ;
-let user_followings ;
-let user_bio ;
-let user_blog ;
-let user_publicRepos ;
-let gitHub_stars ;
-var output;
+let user_picture="";
+let user_name ="";
+let user_location= "";
+let user_profile ="" ;
+let user_followers='' ;
+let user_followings='' ;
+let user_bio='' ;
+let user_blog= '' ;
+let user_publicRepos ='';
+let gitHub_stars ='' ;
+let output;
+console.log(user_bio)
 
 function promptUser() {
     //     return inquirer.prompt([
@@ -42,72 +43,27 @@ function promptUser() {
         ]
     }
     ])
-
-        .then(function ({ username }) {
-            //  const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
-            // });
-            //const queryUrl1 = `https://api.github.com/users/${username}/followers?per_page=100`
-            // const queryUrl2 = `https://api.github.com/users/${username}/following?per_page=100`
-            const queryUrl = `https://api.github.com/users/${username}?per_page=100`
-            axios
-                .get(queryUrl)
-                .then(response => {
-                    //console.log("result:", response)
-
-                     output = response.data;
-                    //console.log("data: ", output)
-                     user_picture =response.data.avatar_url;
-                     user_name = response.data.name;
-                     user_location = response.data.location;
-                     user_profile = response.data.html_url;
-                     user_followers = response.data.followers;
-                     user_followings = response.data.following;
-                     user_bio = response.data.bio;
-                     user_blog = response.data.blog;
-                     user_publicRepos = response.data.public_repos;
-                     gitHub_stars = response.data.public_gists
-
-                    //generateHTML(user_name)
-                    //generateHTML(user_picture)
-
-                    //   const repoNameStr = outputArray.push(`${user_picture},${user_name},${user_location},${user_profile}, ${user_profile}, ${user_followers},${user_followings},${user_bio},
-                    //   ${user_blog},${user_publicRepos}`)
-                    //   //console.log(outputArray)
-                    //   let eachOuput;
-                    //   outputArray.forEach(output=>{
-                    //       eachOuput= JSON.stringify(`${output}, '\n'`)
-
-                })
-
+    .then( function ({username, color}) {
+            
+        const queryUrl = `https://api.github.com/users/${username}?per_page=100`;
+            //const resp= await axios.get(queryUrl);
+    return axios
+     .get(queryUrl).then(resp => {
+         const data = resp.data
+         //console.log("resp:", data)
+            return{
+              githubData: resp,
+              nameColor: color,
+            }    
         })
+    });
 
 }
  
- 
-
-module.exports = {
-    promptUser: promptUser,
-    user_picture: promptUser.user_picture,
-    user_name : promptUser.user_name,
-    user_bio : promptUser.bio,
-    user_profile: promptUser.user_profile,
-    user_location:promptUser.user_location,
-    user_followers:promptUser.user_followers,
-    user_followings:promptUser.user_followings,
-    user_blog:promptUser.user_blog,
-    user_publicRepos:promptUser.user_publicRepos,
-    gitHub_stars:promptUser.gitHub_stars,
-     
-
-};
-console.log(`out:, ${output}`)
-console.log(`out122:, ${user_followings}`)
-
- 
- 
-
-     
-function generateHTML(promptUser) {
+function generateHTML(data, color) {
+    console.log("mydata:", data)
+    //console.log("color:", color)
+    //console.log("newdata:", data)
  return `
    <!DOCTYPE html>
    <html lang="en">
@@ -121,33 +77,33 @@ function generateHTML(promptUser) {
          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
          <title>GitHub Profile</title>
          <style>
-           .color{
-               
+            
+           .nameColor{
+            color: ${color}
+           } 
              
          
-           }
           </style>     
       </head>
       <body>
  
          <div class="jumbotron .container-fluid">
-             <h3 class="display-5 color">${promptUser.user_name}</h3>
+             <h3 class="display-5 nameColor">${gitdata.name}</h3>
              <hr class="my-4">
-             <img src=${promptUser.user_picture} width="200" height="200" alt="user"></img>
+             <img src=${data.avatar_url} width="200" height="200" alt="user"></img>
              </br></br>
-             <p class="lead">Bio: ${promptUser.user_bio}</p>
-             <p class="lead"><a href=${promptUser.user_profile}>${promptUser.user_name}'s GitHub Profile</a></p>
-             <p class="lead"><a href=${promptUser.user_bio}>${promptUser.user_name}'s Blog</a></p>
-             <p class="lead">GitHub Stars: ${promptUser.gitHub_stars}</p>
-             <p class="lead">Public Repos: ${promptUser.user_publicRepos}</p>
-             <p class="lead">Followers: ${promptUser.user_followers}</p>
-             <p class="lead">Following: ${promptUser.user_followings}</p>
-             <p class="lead">Location: ${promptUser.user_location}</p>
+             <p class="lead">Bio: ${data.bio}</p>
+             <p class="lead"><a href=${data.html_url}>${data.name}'s GitHub Profile</a></p>
+             <p class="lead"><a href=${data.blog}>${data.name}'s Blog</a></p>
+             <p class="lead">GitHub Stars: ${data.gists_url}</p>
+             <p class="lead">Public Repos: ${data.public_repos}</p>
+             <p class="lead">Followers: ${data.followers}</p>
+             <p class="lead">Following: ${data.following}</p>
+             <p class="lead">Location: ${data.location}</p>
              
              
              <hr class="my-4">
-             <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-             <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+              
     </div> 
     </body>   
    </html> `
@@ -157,18 +113,17 @@ async function init() {
     try {
 
         const output = await promptUser();
-        console.log("ans:", output)
-        const html = generateHTML(output);
-        await writeFileAsync("index.html", html)
-        console.log("created an HTML page successfully")
+        //console.log("ans:", output)
+        const html = generateHTML(output.githubData , output.nameColor);
+        await writeFileAsync("index.html", html);
+        console.log("created an HTML page successfully");
     }
     catch (err) {
-        console.log(`error: `, err)
+        console.log(`error: `, err);
 
     }
 
 
 }
-
 //promptUser()
 init()
